6bc843c3cd6266e096d604d9727fc27cfebebc4c
hawken <hawken@thehawken.org>
2016-08-23 03:53:44 +0200

    enable stdin guessing

--- gpg-symcrack.c
+++ gpg-symcrack.c
@@ -9,6 +9,34 @@
 #include "gpg-s2k.h"
 #include "gpg-test.h"
 
+int main_test2(int argc, char **argv) {
+	if(argc < 1) {
+		printf("test2 <challenge.bin-in> - Test a passwords from stdin. One per line\n");
+		return 1;
+	}
+	const char *in = argv[0];
+
+	gpg_test_state ts = gpg_test_new(gpg_challenge_read(in));
+
+	char pw[BUFSIZ];
+	do {
+		if(fgets(pw, BUFSIZ, stdin) == NULL)
+			break;
+		int len;
+		for(len = strlen(pw); pw[len-1] == '\n' || pw[len-1] == '\r'; len--) {
+		}
+		pw[len] = 0;
+
+		gpg_test_setpw(&ts, pw);
+		if(gpg_test_test1(&ts)) {
+			printf("Probably correct: '%s'\n", pw);
+		}
+	} while(1);
+
+	gpg_test_delete(&ts);
+
+	return 0;
+}
 int main_test(int argc, char **argv) {
 	if(argc < 2) {
 		printf("test <challenge.bin-in> <password> - Test a password on a challenge file\n");
@@ -18,12 +46,14 @@ int main_test(int argc, char **argv) {
 	const char *pw = argv[1];
 
 	gpg_test_state ts = gpg_test_new(gpg_challenge_read(in));
+
 	gpg_test_setpw(&ts, pw);
 	if(gpg_test_test1(&ts)) {
 		printf("Probably correct password\n");
 	} else {
 		printf("Wrong password\n");
 	}
+
 	gpg_test_delete(&ts);
 
 	return 0;
@@ -45,6 +75,7 @@ int help(const char *program) {
 	printf("USAGE: %s <action>\n", program);
 	printf("\t"); main_convert(0, NULL);
 	printf("\t"); main_test(0, NULL);
+	printf("\t"); main_test2(0, NULL);
 	return 1;
 }
 int main(int argc, char **argv) {
@@ -56,6 +87,8 @@ int main(int argc, char **argv) {
 		return main_convert(argc-2, argv+2);
 	else if(!strcmp(argv[1], "test"))
 		return main_test(argc-2, argv+2);
+	else if(!strcmp(argv[1], "test2"))
+		return main_test2(argc-2, argv+2);
 	else
 		return help(argv[0]);
 }
